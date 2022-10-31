e32075094f0c49bc8dc2d531c4dc934563ad9fcd
Felice <86122274+GITBUG123@users.noreply.github.com>
2022-10-31 17:48:22 +0700

    Delete 2440094516-GSLC1BladeFeature directory

--- 2440094516-GSLC1BladeFeature/views/layout/footer.blade.php
+++
@@ -1,3 +0,0 @@
-<footer class="bg-dark justify-content-center">
-    <h1 class="p-3">&copy; Copyright 2022 || 2440094516 - Felice Cendekia ||</h1>
-</footer>
--- 2440094516-GSLC1BladeFeature/views/layout/navbar.blade.php
+++
@@ -1,9 +0,0 @@
-<nav class="navbar navbar-dark bg-dark sticky-top">
-    <div class="container-fluid d-flex justify-content-start">
-        <a class="navbar-brand" href="/">
-            <img src="https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/global/dota2_logo_horiz.png" alt="Dota 2"
-            width="230px" height="50px">
-        </a>
-        <a class="nav-link text-bg-dark" aria-current="page" href="/">HEROES</a>
-    </div>
-  </nav>
