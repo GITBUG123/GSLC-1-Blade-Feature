6b8b0c4221a68920da5559c7af32205e7314a5d7
Felice <86122274+GITBUG123@users.noreply.github.com>
2022-11-09 23:52:27 +0700

    Update dotaHero.php

--- app/Models/dotaHero.php
+++ app/Models/dotaHero.php
@@ -4,9 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class dotaHero extends Model
 {
+    use SoftDeletes;
     use HasFactory;
     protected $table = 'heros';
+    protected $dates = ['deleted_at'];
 }
