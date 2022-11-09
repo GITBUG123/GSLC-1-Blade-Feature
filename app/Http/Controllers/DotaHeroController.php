f8c523dd1c5dbf1f9624b2809f343623fc42bbf5
Felice <86122274+GITBUG123@users.noreply.github.com>
2022-11-09 23:52:08 +0700

    Update DotaHeroController.php

--- app/Http/Controllers/DotaHeroController.php
+++ app/Http/Controllers/DotaHeroController.php
@@ -8,7 +8,27 @@ use Illuminate\Http\Request;
 class DotaHeroController extends Controller
 {
     public function index(){
-        $dotaherolist = dotaHero::all();
-        return view('dota', compact('dotaherolist'));
+        // $dotahero = dotaHero::all();
+        // return view('dota', compact('dotahero'));
+        $dotahero = dotahero::latest()->paginate(4);
+        return view('dota', compact('dotahero'))->with('i', (request()->input('page', 1) - 1) * 4);
+    }
+
+    public function index2(){
+        $dotahero = dotahero::whereNotNull('id')->get();
+        // $dotahero = dotaHero::all();
+        // return view('dotaherolist', compact('dotahero'));
+        $dotahero = dotahero::latest()->paginate(5);
+        return view('dotaherolist', compact('dotahero'))->with('i', (request()->input('page', 1) - 1) * 5);
+    }
+
+    public function deleteHero($id){
+        $dotahero = dotaHero::find($id);
+
+        if(isset($dotahero)){
+            $dotahero->delete();
+        }
+
+        return redirect()->back();
     }
 }
