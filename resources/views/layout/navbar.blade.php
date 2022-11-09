b92066baf126c967436ac3abc0cb78e4593c74a3
Felice <86122274+GITBUG123@users.noreply.github.com>
2022-11-09 23:53:58 +0700

    Update navbar.blade.php

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -1,9 +1,13 @@
 <nav class="navbar navbar-dark bg-dark sticky-top">
-    <div class="container-fluid d-flex justify-content-start">
+    <div class="container-fluid d-flex justify-content-between">
         <a class="navbar-brand" href="/">
             <img src="https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/global/dota2_logo_horiz.png" alt="Dota 2"
             width="230px" height="50px">
         </a>
-        <a class="nav-link text-bg-dark" aria-current="page" href="/">HEROES</a>
+        <div class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link text-bg-dark" aria-current="page" href="/">HOME</a>
+            </li>
+        </div>
     </div>
   </nav>
